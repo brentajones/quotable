9a9dfb9bdcd8e273815b949c53947aa1378c942e
Brent Jones <bjones@stlpublicradio.org>
2015-01-29 15:33:34 -0600

    update for The Listening Project

--- app_config.py
+++ app_config.py
@@ -22,7 +22,7 @@ PROJECT_SLUG = 'quotable'
 
 # The name of the repository containing the source
 REPOSITORY_NAME = 'quotable'
-REPOSITORY_URL = 'git@github.com:nprapps/%s.git' % REPOSITORY_NAME
+REPOSITORY_URL = 'git@github.com:brentajones/%s.git' % REPOSITORY_NAME
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
 # The name to be used in paths on the server
@@ -31,9 +31,9 @@ PROJECT_FILENAME = 'quotable'
 """
 DEPLOYMENT
 """
-FILE_SERVER = 'tools.apps.npr.org'
-S3_BUCKET = 'tools.apps.npr.org'
-ASSETS_S3_BUCKET = 'assets.apps.npr.org'
+FILE_SERVER = 'apps.stlpublicradio.org'
+S3_BUCKET = 'apps.stlpublicradio.org'
+ASSETS_S3_BUCKET = 'assets.apps.stlpublicradio.org'
 
 # These variables will be set at runtime. See configure_targets() below
 DEBUG = True
@@ -71,15 +71,8 @@ GOOGLE = {
     'IMAGE_URL': TWITTER['IMAGE_URL']
 }
 
-NPR_DFP = {
-    'STORY_ID': '203618536',
-    'TARGET': 'News_NPR_News_Investigations',
-    'ENVIRONMENT': 'NPRTEST',
-    'TESTSERVER': 'true'
-}
-
 """
 SERVICES
 """
-GOOGLE_ANALYTICS_ID = 'UA-5828686-4'
+GOOGLE_ANALYTICS_ID = ''
 
--- www/js/app.js
+++ www/js/app.js
@@ -12,17 +12,9 @@ var $logoWrapper = null;
 
 var quotes = [
     {
-        "quote": "I'd been drinking.",
-        "source": "Dennis Rodman"
-    },
-    {
         "quote": "I've made a huge mistake.",
-        "source": "G.O.B."
-    },
-    {
-        "quote": "Yes, I have smoked crack cocaine",
-        "source": "Toronto Mayor Rob Ford",
-        "size": 65
+        "source": "G.O.B.",
+		"size": 80 
     },
     {
         "quote": "Annyong.",
@@ -33,11 +25,6 @@ var quotes = [
         "quote": "STEVE HOLT!",
         "source": "Steve Holt",
         "size": 65
-    },
-    {
-        "quote": "Whoa, whoa, whoa. There's still plenty of meat on that bone. Now you take this home, throw it in a pot, add some broth, a potato. Baby, you've got a stew going.",
-        "source": "Carl Weathers",
-        "size": 40
     }
 ];
 
